7c84053dd29debeef9403bf025523fbd96043986
Pipplip <72355594+Pipplip@users.noreply.github.com>
2023-10-03 22:34:51 +0200

    Create moviepy Project

---
+++ Projekte/moviepy/moviepy_application.py
@@ -0,0 +1,25 @@
+from moviepy.editor import *
+
+def load_video():
+    # loading video
+    clip = VideoFileClip("D:/Desktop/A22.avi")
+    print("duration: "+str(clip.duration)+"s")
+
+    # getting subclip as video is large
+    clip = clip.subclip(0, 10) # Zeitspanne eingeben
+    return clip
+
+def rotate_video(degree):
+    clip = load_video()
+    clip = clip.rotate(int(degree))
+    return clip
+
+def show_video(clip):
+    clip.preview(fps = 20)
+
+if __name__ == "__main__":
+    print("start")
+    video = rotate_video(90)
+    show_video(video)
+    print("end")
+
